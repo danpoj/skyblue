2d25a31a0f107b5773439108281645903f8fee2a
danpoj <jws970306@khu.ac.kr>
2024-06-23 17:01:29 +0900

    fix image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'pub-91d41fc69e574b6b8e7b0ca20fdb95b7.r2.dev',
+        protocol: 'https',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 export default nextConfig;
